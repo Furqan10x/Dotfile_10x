bb3175eae58f0122993b32479a97e2a9754c9d18
Furqan10x <furqan@10xengineers.com>
2024-01-23 15:53:28 +0500

    [commit message]

---
+++ install.sh
@@ -0,0 +1,9 @@
+# In ~/dotfiles/install.sh
+#!/bin/bash
+
+# Symlink dotfiles to home directory
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# Add more lines for other dotfiles
+
+echo "Dotfiles installation complete."
+
